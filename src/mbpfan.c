f53c316d74f1ea0239120a8d382f0d7df0dd4c30
networkException <git@nwex.de>
2020-09-05 20:10:16 +0200

    AppleSMC: Ajusted path

--- src/mbpfan.c
+++ src/mbpfan.c
@@ -51,7 +51,7 @@
 #define max(a,b) ((a) > (b) ? (a) : (b))
 
 #define CORETEMP_PATH "/sys/devices/platform/coretemp.0"
-#define APPLESMC_PATH "/sys/devices/platform/applesmc.768"
+#define APPLESMC_PATH "/sys/devices/LNXSYSTM:00/LNXSYBUS:00/PNP0A08:00/device:96/APP0001:00"
 
 /* temperature thresholds
  * low_temp - temperature below which fan speed will be at minimum
@@ -274,7 +274,7 @@ t_fans *retrieve_fans()
     char *path_fan_max = NULL;
     char *path_fan_min = NULL;
 
-    const char *path_begin = "/sys/devices/platform/applesmc.768/fan";
+    const char *path_begin = "/sys/devices/LNXSYSTM:00/LNXSYBUS:00/PNP0A08:00/device:96/APP0001:00/fan";
     const char *path_output_end = "_output";
     const char *path_label_end = "_label";
     const char *path_man_end = "_manual";
